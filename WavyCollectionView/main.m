c3a9d9ded179849685a21bb7ea512ac385487fab
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-04-25 16:36:09 -0400

    Initial Commit

---
+++ WavyCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WavyCollectionView
+//
+//  Created by Alejandro Zielinsky on 2018-04-25.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
